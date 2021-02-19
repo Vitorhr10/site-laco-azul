7246e0b71f498f9a150cf98925764b0c68070f06
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-02-18 23:39:06 -0300

    feat: Services component

--- src/constants/services.js
+++ src/constants/services.js
@@ -4,19 +4,19 @@ export default [
   {
     id: 1,
     icon: <FaCode className="service-icon" />,
-    title: "web development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "cuidados especiais",
+    text: `Cuidamos da sua criança como se fosse o nossa.`,
   },
   {
     id: 2,
     icon: <FaSketch className="service-icon" />,
-    title: "web design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "fisioterapia",
+    text: `Temos vários profissionais capacitados para cuidar das sessões.`,
   },
   {
     id: 3,
     icon: <FaAndroid className="service-icon" />,
-    title: "app design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    title: "atividades físicas",
+    text: `Temos várias atividades físicas disponíveis para a criança escolher a qual desejar.`,
   },
 ]
