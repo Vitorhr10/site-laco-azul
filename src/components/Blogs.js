61bcc5f2c340fe62476053c156623ce170c8be6e
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-03-04 23:58:00 -0300

    feat: News page with all data from strapi

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -11,7 +11,7 @@ export const Blogs = ({blogs, title, showLink}) => {
             return <Blog key={blog.id} {...blog}/>
           })}
         </div>
-        {showLink && (<Link to="/blog" className="btn center-btn">blog</Link>)}
+        {showLink && (<Link to="/blog" className="btn center-btn">notícias</Link>)}
       
     </section>
   )
