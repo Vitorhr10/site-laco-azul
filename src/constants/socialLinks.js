abecb8210d062f5369d291b3a9259806fa6722e5
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-03-11 23:41:20 -0300

    fix: SocialLinks center

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,37 +1,19 @@
 import React from "react"
 import {
   FaFacebookSquare,
-  FaLinkedin,
-  FaDribbbleSquare,
-  FaBehanceSquare,
-  FaTwitterSquare,
+  FaInstagramSquare,
 } from "react-icons/fa"
 
 const data = [
   {
     id: 1,
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.facebook.com/grupolacoazul",
   },
   {
     id: 2,
-    icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
-  },
-  {
-    id: 3,
-    icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://www.twitter.com",
-  },
-  {
-    id: 4,
-    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: "https://www.twitter.com",
-  },
-  {
-    id: 5,
-    icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-    url: "https://www.twitter.com",
+    icon: <FaInstagramSquare className="social-icon"></FaInstagramSquare>,
+    url: "https://www.instagram.com/?hl=pt-br",
   },
 ]
 const links = data.map(link => {
