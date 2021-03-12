d3bc3285a77e1d9ce41803d7db9b33a1ee4e1831
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-03-12 00:20:55 -0300

    feat: About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,40 @@ import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Title from "../components/Title"
 import Image from "gatsby-image"
-// ...GatsbyImageSharpFluid
-const About = () => {
-  return <h2>about page</h2>
+const About = ({data:{ about: { nodes },},}) => {
+  const{title,info,image} = nodes[0]
+
+  return (
+    <Layout>
+      <section className="about-page">
+        <div className="section-center about-center">
+          <Image fluid={image.childImageSharp.fluid} className="about-img" />
+          <article className="about-text">
+            <Title title={title} />
+            <p>{info}</p>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+{
+  about:allStrapiAbout {
+    nodes {
+      title
+      info
+      image {
+        childImageSharp {
+          fluid {
+              ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  }
+}
+`
 
 export default About
