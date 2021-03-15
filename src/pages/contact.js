75fdcd3a9d86349abba48b5b54897c8ed9fe0f4e
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-03-14 23:59:21 -0300

    fix: Placeholder names

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,9 +11,9 @@ const contact = () => {
           <h3>Entrar em contato</h3>
           <form action="https://formspree.io/f/mbjpnpql" method="POST">
             <div className="form-group">
-              <input type="text" name="name" placeholder="name" className="form-control" />
+              <input type="text" name="name" placeholder="nome" className="form-control" />
               <input type="email" name="email" placeholder="email" className="form-control" />
-              <textarea name="message" rows="5" placeholder="message" className="form-control"></textarea>
+              <textarea name="message" rows="5" placeholder="mensagem" className="form-control"></textarea>
             </div>
             <button type="submit" className="submit-btn btn">
               Enviar
