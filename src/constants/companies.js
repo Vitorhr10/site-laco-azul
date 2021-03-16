959ba4c9fa97f72926e1f27b808b48d32d9c7bea
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-03-16 04:05:08 -0300

    fix: Company values color on hover

--- src/constants/companies.js
+++ src/constants/companies.js
@@ -3,19 +3,19 @@ import { FaChild, FaEye, FaHandHoldingHeart } from "react-icons/fa"
 export default [
   {
     id: 1,
-    icon: <FaChild className="service-icon" />,
+    icon: <FaChild className="company-icon" />,
     title: "missão",
     text: `Promover ações que contribuam para a formação de jovens cidadãos, ativos e comprometidos com a construção de uma sociedade mais justa e participativa.`,
   },
   {
     id: 2,
-    icon: <FaEye className="service-icon" />,
+    icon: <FaEye className="company-icon" />,
     title: "visão",
     text: `Ter uma comunidade ativa na transformação de sua realidade. Ter uma comunidade ativa na transformação de sua realidade.`,
   },
   {
     id: 3,
-    icon: <FaHandHoldingHeart className="service-icon" />,
+    icon: <FaHandHoldingHeart className="company-icon" />,
     title: "valores",
     text: `Agimos como cidadãos responsáveis, trabalhando para gerar riqueza, priorizando o respeito aos colaboradores, investidores, fornecedores e clientes.`,
   },
